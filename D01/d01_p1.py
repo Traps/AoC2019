d08f2a727f9cbc13a214486c67b38af02b554946
Traps <trapsnegel@gmail.com>
2019-12-09 12:28:55 +0100

    Solutions for part 1 & 2 of day 1

---
+++ D01/d01_p1.py
@@ -0,0 +1,13 @@
+from math import floor
+
+input_file = open('D01/d01.input', 'r')
+
+input_text = input_file.read()
+
+input_file.close()
+
+module_masses = [int(m_string) for m_string in input_text.splitlines()]
+
+fuel_sum = sum([floor(mass / 3) - 2 for mass in module_masses])
+
+print('Fuel needed:', fuel_sum)
---
+++ D01/d01_p2.py
@@ -0,0 +1,20 @@
+from math import floor
+
+input_file = open('D01/d01.input', 'r')
+
+input_text = input_file.read()
+
+input_file.close()
+
+module_masses = [int(m_string) for m_string in input_text.splitlines()]
+
+fuel_sum = 0
+for mass in module_masses:
+    fuel_needed = floor(mass / 3) - 2
+
+    if fuel_needed > 0:
+        fuel_sum += fuel_needed
+
+        module_masses.append(fuel_needed)
+
+print('Fuel needed:', fuel_sum)
