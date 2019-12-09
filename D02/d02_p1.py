5fe561381c03083b4b17de59df0f3203e64fda27
Traps <trapsnegel@gmail.com>
2019-12-09 15:16:43 +0100

    Restructured day 2 part 1

--- D02/d02_p1.py
+++ D02/d02_p1.py
@@ -1,3 +1,21 @@
+def run_intcode(intcode):
+    for i_position in range(0, len(intcode), 4):
+        op, i_r1, i_r2, i_w = intcode[i_position:(i_position+4)]
+
+        if op == 99:
+            return intcode[0]
+
+        elif op == 1:
+            intcode[i_w] = intcode[i_r1] + intcode[i_r2]
+
+        elif op == 2:
+            intcode[i_w] = intcode[i_r1] * intcode[i_r2]
+
+        else:
+            return -1
+
+    return -1
+
 input_file = open('D02/.input', 'r')
 
 input_text = input_file.read()
@@ -8,16 +26,4 @@ program = [int(s) for s in input_text.split(',')]
 
 program[1:3] = [12, 2]
 
-for i_position in range(0, len(program), 4):
-    op, i_r1, i_r2, i_w = program[i_position:(i_position+4)]
-
-    if op == 99:
-        break
-
-    elif op == 1:
-        program[i_w] = program[i_r1] + program[i_r2]
-
-    elif op == 2:
-        program[i_w] = program[i_r1] * program[i_r2]
-
-print('Program output:',program[0])
+print('Program output:', run_intcode(program))
